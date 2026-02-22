1ec6e9865654bf7e8ba78a5344765db7d3db9442
tsspfaws-eng <tsspf.aws@gmail.com>
2026-02-23 01:18:26 +0530

    Add import statement for styled component

--- frontend/src/components/layouts/DashboardLayout.js
+++ frontend/src/components/layouts/DashboardLayout.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { styled, useTheme } from '@mui/material/styles';
+
 import {
   Box,
   Drawer,
